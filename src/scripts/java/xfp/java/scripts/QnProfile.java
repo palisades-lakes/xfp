477c38d5aaed8d4a95a4f5022d358790adc43cf0
John McDonald <palisades.lakes@gmail.com>
2019-02-20 21:57:59 -0800

    prepare for profiling

--- src/scripts/java/xfp/java/scripts/QnProfile.java
+++ src/scripts/java/xfp/java/scripts/QnProfile.java
@@ -2,24 +2,27 @@ package xfp.java.scripts;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
+import java.util.Map;
 import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
+import xfp.java.algebra.OneSetOneOperation;
 import xfp.java.algebra.OneSetTwoOperations;
+import xfp.java.algebra.Set;
 import xfp.java.algebra.TwoSetsTwoOperations;
 import xfp.java.prng.PRNG;
 import xfp.java.prng.Seeds;
-import xfp.java.test.algebra.AlgebraicStructureTests;
 import xfp.java.test.algebra.SetTests;
 
 //----------------------------------------------------------------
 /** Profiling rational vector spaces. 
  *
- * jy ----source 11 src/scripts/java/xfp/java/scripts/QnProfile.java
+ * jy --source 11 src/scripts/java/xfp/java/scripts/QnProfile.java
  * 
  * @author palisades dot lakes at gmail dot com
- * @version 2019-01-14
+ * @version 2019-02-20
  */
 
 @SuppressWarnings("unchecked")
@@ -28,29 +31,35 @@ public final class QnProfile {
   private static final int TRYS = 1023;
 
   private static final void 
-  linearspaceTests (final TwoSetsTwoOperations space) {
-
-    final OneSetTwoOperations scalars = 
-      (OneSetTwoOperations) space.scalars();
-    AlgebraicStructureTests.fieldTests(scalars);
+  twoSetsTwoOperationsTests (final TwoSetsTwoOperations space) {
 
     SetTests.tests(space);
-
+    
+    final Map<Set,List> lawLists = space.linearSpaceLaws();
+    
+    final OneSetOneOperation elements = 
+      (OneSetOneOperation) space.elements();
+    final Supplier vg = 
+      space.elements().generator( 
+        PRNG.well44497b(
+          Seeds.seed("seeds/Well44497b-2019-01-09.txt")));
+    for(final Object law : lawLists.get(elements)) {
+      for (int i=0; i<TRYS; i++) {
+        assertTrue(((Predicate) law).test(vg));} } 
+    
+    final OneSetTwoOperations scalars = 
+      (OneSetTwoOperations) space.scalars();
     final Supplier sg = 
       space.scalars().generator( 
         PRNG.well44497b(
           Seeds.seed("seeds/Well44497b-2019-01-11.txt")));
-    final Supplier vg = 
-      space.generator( 
-        PRNG.well44497b(
-          Seeds.seed("seeds/Well44497b-2019-01-09.txt")));
-
-    for(final Object law : space.linearSpaceLaws()) {
+    for(final Object law : lawLists.get(scalars)) {
       for (int i=0; i<TRYS; i++) {
-        if (law instanceof Predicate) {
-          assertTrue(((Predicate) law).test(vg)); }
-        else if (law instanceof BiPredicate) {
-          assertTrue(((BiPredicate) law).test(vg,sg));} } } }
+        assertTrue(((Predicate) law).test(sg));} } 
+  
+    for(final Object law : lawLists.get(space)) {
+      for (int i=0; i<TRYS; i++) {
+        assertTrue(((BiPredicate) law).test(vg,sg));} } }
 
   //--------------------------------------------------------------
 
@@ -58,8 +67,8 @@ public final class QnProfile {
     for (final int n : new int[] { 1, 3, 13, 127, 1023}) {
       System.out.println(n);
       final TwoSetsTwoOperations qn = 
-        TwoSetsTwoOperations.getBFn(n);
-      linearspaceTests(qn); } }
+        TwoSetsTwoOperations.getQn(n);
+      twoSetsTwoOperationsTests(qn); } }
 
   //--------------------------------------------------------------
 }
